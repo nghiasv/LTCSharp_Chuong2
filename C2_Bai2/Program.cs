56829e4e7c10c7fc1ffa6990ab4445f486907353
nghiasv <doankimnghia1908@gmail.com>
2025-02-27 09:24:13 +0700

    Lan2

--- C2_Bai2/Program.cs
+++ C2_Bai2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    enum Mau
+    {
+        Do = 1,
+        Xanh = 2,
+        Vang = 3,
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
