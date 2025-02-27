f25f0f7b40d1467f836d37881a9d2c0d932ee8b8
nghiasv <doankimnghia1908@gmail.com>
2025-02-27 09:27:25 +0700

    Lan3

--- C2_Bai2/Program.cs
+++ C2_Bai2/Program.cs
@@ -2,12 +2,12 @@
 {
     enum Mau
     {
-        Do = 1,
-        Xanh = 2,
-        Vang = 3,
+        Do232 = 1,
+        Xanh232 = 2,
+        Vang232 = 3,
     }
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(Mau.Do232);
     }
 }
--- C2_Bai2/obj/Debug/net8.0/C2_Bai2.AssemblyInfo.cs
+++ C2_Bai2/obj/Debug/net8.0/C2_Bai2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C2_Bai2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56829e4e7c10c7fc1ffa6990ab4445f486907353")]
 [assembly: System.Reflection.AssemblyProductAttribute("C2_Bai2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C2_Bai2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
