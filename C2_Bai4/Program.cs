86c237f9aa19e337ab81265825984a27dbb93814
nghiasv <doankimnghia1908@gmail.com>
2025-02-27 09:59:00 +0700

    Lan2

--- C2_Bai4/Program.cs
+++ C2_Bai4/Program.cs
@@ -2,6 +2,13 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Nhập số lượng phần tử: ");
+        int n232 = int.Parse(Console.ReadLine());
+        int[] numbers = new int[n232];
+        for (int i232 = 0; i232 < n232; i232++)
+        {
+            Console.Write($"Nhập phần tử thứ {i232 + 1}: ");
+            numbers[i232] = int.Parse(Console.ReadLine());
+        }
     }
 }
