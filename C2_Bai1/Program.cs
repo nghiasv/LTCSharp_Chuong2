1c50c975003700822763a3bdbaee0c0dcc3566da
nghiasv <doankimnghia1908@gmail.com>
2025-02-27 09:04:24 +0700

    Lan2

--- C2_Bai1/Program.cs
+++ C2_Bai1/Program.cs
@@ -1,5 +1,17 @@
 ﻿internal class Program
 {
+    static public void Sinhvien(string HoTen232,
+                           string MSV232,
+                           int Tuoi232,
+     string Nganh232 = "Cong nghe thong tin")
+
+    {
+        Console.WriteLine("HoTen: {0}", HoTen232);
+        Console.WriteLine("MSV: {0}", MSV232);
+        Console.WriteLine("Tuoi: {0}", Tuoi232);
+        Console.WriteLine("Nghanh: {0}", Nganh232);
+    }
+
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
--- C2_Bai1/obj/Debug/net8.0/C2_Bai1.AssemblyInfo.cs
+++ C2_Bai1/obj/Debug/net8.0/C2_Bai1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C2_Bai1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e2ff1f48e7aae23ad299521c145345c7328f44e")]
 [assembly: System.Reflection.AssemblyProductAttribute("C2_Bai1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C2_Bai1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
