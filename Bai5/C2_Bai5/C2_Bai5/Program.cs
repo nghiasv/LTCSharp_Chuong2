3e9719de4aab79954f1c48d3ac4b271d116d4ab4
nghiasv <doankimnghia1908@gmail.com>
2025-02-27 10:17:41 +0700

    Lan2

--- Bai5/C2_Bai5/C2_Bai5/Program.cs
+++ Bai5/C2_Bai5/C2_Bai5/Program.cs
@@ -1,5 +1,27 @@
 ﻿internal class Program
 {
+    enum PointColor
+    {
+        LightBlue,
+        BloodRed,
+        Gold
+    }
+    class Point
+    {
+        public int X232 { get; set; }
+        public int Y232 { get; set; }
+        public PointColor Color232 { get; set; }
+        public Point(int x, int y, PointColor color)
+        {
+            X232 = x;
+            Y232 = y;
+            Color232 = color;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Point({X232}, {Y232}) - Color: {Color232}");
+        }
+    }
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
