be60043e94f6ae1c9e2907847348c47b3c34b60f
nghiasv <doankimnghia1908@gmail.com>
2025-02-27 10:05:35 +0700

    Lan3

--- C2_Bai4/Program.cs
+++ C2_Bai4/Program.cs
@@ -1,14 +1,26 @@
-﻿internal class Program
+﻿using System.Text;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        Console.OutputEncoding = Encoding.UTF8;
         Console.Write("Nhập số lượng phần tử: ");
         int n232 = int.Parse(Console.ReadLine());
-        int[] numbers = new int[n232];
+        int[] ds232 = new int[n232];
         for (int i232 = 0; i232 < n232; i232++)
         {
             Console.Write($"Nhập phần tử thứ {i232 + 1}: ");
-            numbers[i232] = int.Parse(Console.ReadLine());
+            ds232[i232] = int.Parse(Console.ReadLine());
         }
+        Console.WriteLine("Các số chẵn trong mảng là:");
+        foreach (int num232 in ds232) 
+        {
+            if (num232 % 2 == 0)
+            {
+                Console.Write(num232 + " ");
+            }
+        }
+        Console.WriteLine("\nTạm Biệt!");
     }
 }
